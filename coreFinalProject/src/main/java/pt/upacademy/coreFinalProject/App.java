c49c883beb29ead2c5ebe5d698656345167e570b
miguelramirezcordeiro <miguelramirezcordeiro@gmail.com>
2020-01-16 12:13:04 +0000

    init coreBackend

---
+++ coreFinalProject/src/main/java/pt/upacademy/coreFinalProject/App.java
@@ -0,0 +1,9 @@
+package pt.upacademy.coreFinalProject;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class App extends Application {
+
+}
