8ee62b0f70d55bf9815fd69e2ba156ab16be3da8
miguelramirezcordeiro <miguelramirezcordeiro@gmail.com>
2020-01-17 14:50:24 +0000

    fix EntityConverter

--- src/main/java/pt/upacademy/coreFinalProject/models/converters/EntityConverter.java
+++ src/main/java/pt/upacademy/coreFinalProject/models/converters/EntityConverter.java
@@ -2,8 +2,11 @@ package pt.upacademy.coreFinalProject.models.converters;
 
 import javax.enterprise.context.RequestScoped;
 
+import pt.upacademy.coreFinalProject.models.EntityRoot;
+import pt.upacademy.coreFinalProject.models.DTOS.EntityDTO;
+
 @RequestScoped
-public abstract class EntityConverter<E, D> {
+public abstract class EntityConverter<E extends EntityRoot, D extends EntityDTO> {
 	
 	public abstract E toEntity(D dto);
 	
